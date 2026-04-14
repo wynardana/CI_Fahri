697b03e79a0f38588c6638d52f18f8fa46acd3cd
Fahri Ilham Muzzaki <54172080+Fuxzi@users.noreply.github.com>
2026-04-14 19:49:06 +0700

    Added DataTables

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,11 +2,11 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Perpustakaan - CI3 SB admin</title>
+        <title>UTS - CI3 SB admin</title>
         <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
          <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
           <!-- <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet"> -->
-
+        <link href="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css')?>" rel="stylesheet">
 </head>
 <body id="page-top">
     <div id="wrapper">
